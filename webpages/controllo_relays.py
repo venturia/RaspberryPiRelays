8943b496fa229cdbf38d3ef39eafb91210ab06d2
Andrea Venturi <andrea.venturi@cern.ch>
2017-04-16 15:57:37 +0200

    aggiunto interfaccia web per controllare i relays

---
+++ webpages/controllo_relays.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+import socket
+import cgi
+import cgitb
+cgitb.enable()
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 5002))
+if len(sys.argv) > 1:
+   client_socket.send(sys.argv[1])
+
+print "Content-type: text/html\n\n"
+print sys.argv[1]," eseguito"
+
