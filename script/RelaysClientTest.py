4cda049c5e7468db5865fde2947042e2d79a8e8d
venturia <andrea.venturi@cern.ch>
2017-04-23 14:53:41 +0200

    Aggiunta la possibilita di configurare porta e nodo remoto

--- script/RelaysClientTest.py
+++ script/RelaysClientTest.py
@@ -4,18 +4,33 @@ import sys
 import socket
 import cgi
 import cgitb
+import getopt
 cgitb.enable()
 
+port=5002
+host='localhost'
+
+try:
+   opts,args = getopt.getopt(sys.argv[1:],"r:p:")
+except getopt.GetoptError:
+   print 'RelaysClientTest -r <remote host> -p <port number>'
+   sys.exit(2)
+for opt,arg in opts:
+   if opt == '-p':
+      port = int(arg)
+   if opt == '-r':
+      host = arg
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 5002))
-if len(sys.argv) > 1:
-   client_socket.send(sys.argv[1])
+client_socket.connect((host, port))
+if len(args) > 0:
+   client_socket.send(args[0])
    reply=[]
    replychunk = client_socket.recv(16)
    while len(replychunk):
      reply.append(replychunk)
      replychunk = client_socket.recv(16)
    print "Content-type: text/html\n\n"
-   print "Risposta server: ",''.join(reply)
+   print "Risposta server: \n",''.join(reply)
 client_socket.close()
 
