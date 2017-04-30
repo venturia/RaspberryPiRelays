49484fc7e5ddb2c3690e80e3d2bbd518e7da0210
Andrea Venturi <andrea.venturi@cern.ch>
2017-04-30 22:08:24 +0200

    Aggiunta script python per accendere e spegnere da interfccia web e modifica html interfaccia web

---
+++ webpages/accensione_relays.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('/home/pi/RaspberryRelays/script')
+import socket
+import cgi
+import cgitb
+import relaylib
+cgitb.enable()
+
+print "Content-type: text/html\n\n"
+if len(sys.argv) > 1:
+   print "Risposta server: ",relaylib.accendi("localhost",5002,int(sys.argv[1]))
+else: 
+   print "Nessun argomento per il comando di accensione"
+
