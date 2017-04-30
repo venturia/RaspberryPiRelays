300c2c5a3327356f061298391de8a9276e0d0de2
Andrea Venturi <andrea.venturi@cern.ch>
2017-04-30 22:10:39 +0200

    Output dello script python stato_relays.py modificato per creare i pulsanti nell'interfaccia web

--- webpages/stato_relays.py
+++ webpages/stato_relays.py
@@ -2,6 +2,8 @@
 
 
 import sys
+import StringIO
+
 sys.path.append('/home/pi/RaspberryRelays/script')
 import socket
 import cgi
@@ -9,6 +11,24 @@ import cgitb
 import relaylib
 cgitb.enable()
 
+statusstring = StringIO.StringIO()
+
+reply,statusdict = relaylib.stato("localhost",5002)
+for gpioch,name,status,lock in zip(statusdict['gpioch'],statusdict['name'],statusdict['status'],statusdict['locked']):
+   disabled=""
+   if lock == 1:
+      disabled="disabled"
+   if status == 1:
+      print >>statusstring, "(GPIO %d) %s  <b><font color='red'>ON</font></b>" % (gpioch,name)
+   else:
+      print >>statusstring, "(GPIO %d) %s  <font color='grey'>OFF</font>" % (gpioch,name)
+ 
+   print >>statusstring, "<button %s onclick='accensione_stato(%d)'>Accendi</button>" % (disabled,gpioch)
+   print >>statusstring, "<button %s onclick='spegnimento_stato(%d)'>Spegni</button><br>" % (disabled,gpioch)
+
 print "Content-type: text/html\n\n"
-print relaylib.stato("localhost",5002)
+if len(statusstring.getvalue())>0:
+   print statusstring.getvalue()
+else:
+    print reply
 
