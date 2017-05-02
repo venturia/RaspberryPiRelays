b88d67c4fc1f37c9f3cbd029668f57a511d6a7fd
Andrea Venturi <andrea.venturi@cern.ch>
2017-05-03 01:02:08 +0200

    Aggiunto bottone per configurare la bit mask

---
+++ webpages/configura_bitmask.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('/home/pi/RaspberryPiRelays/script')
+import socket
+import cgi
+import cgitb
+import relaylib
+cgitb.enable()
+
+form=cgi.FieldStorage()
+
+print "Content-type: text/html\n\n"
+if len(form.getvalue('gpioch')) > 0:
+   print "Risposta server: ",relaylib.configura_bit(form.getvalue('host'),int(form.getvalue('port')),int(form.getvalue('gpioch')),int(form.getvalue('bitmask')))
+else: 
+   print "Nessun argomento per il comando di configurazione bit mask"
+
--- webpages/stato_relays.py
+++ webpages/stato_relays.py
@@ -16,7 +16,7 @@ form=cgi.FieldStorage()
 
 print "Content-type: text/html\n\n"
 reply,statusdict = relaylib.stato(form.getvalue('host'),int(form.getvalue('port')))
-for gpioch,name,status,lock in zip(statusdict['gpioch'],statusdict['name'],statusdict['status'],statusdict['locked']):
+for gpioch,name,status,lock,nbits,bitstatus in zip(statusdict['gpioch'],statusdict['name'],statusdict['status'],statusdict['locked'],statusdict['nbits'],statusdict['bitstatus']):
      disabled=""
      if lock == 1:
         disabled="disabled"
@@ -26,7 +26,15 @@ for gpioch,name,status,lock in zip(statusdict['gpioch'],statusdict['name'],statu
         print >>statusstring, "(GPIO %d) %s  <font color='grey'>OFF</font>" % (gpioch,name)
  
      print >>statusstring, "<button %s onclick='accensione(%d)'>Accendi</button>" % (disabled,gpioch)
-     print >>statusstring, "<button %s onclick='spegnimento(%d)'>Spegni</button><br>" % (disabled,gpioch)
+     print >>statusstring, "<button %s onclick='spegnimento(%d)'>Spegni</button>" % (disabled,gpioch)
+     if nbits > 1:
+        for i in range(nbits):
+           checked=""
+           if bitstatus & 2**i == 2**i:
+              checked="checked"
+           print >>statusstring, "<input type='checkbox' id='%d_%d' value=%d %s %s> %d " % (gpioch,i,2**i,checked,disabled,i)
+        print >>statusstring, "<button %s onclick='configurazione_bitmask(%d,%d)'>Configura</button>" % (disabled,gpioch,nbits)
+     print >>statusstring, "<br>"
 
 if len(statusstring.getvalue())>0:
      print statusstring.getvalue()
